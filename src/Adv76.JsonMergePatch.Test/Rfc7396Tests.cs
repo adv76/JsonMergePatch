8b807797897606a0be4b11fe6c8ea43452b7e264
adv76 <mail@adv76.com>
2026-06-29 22:58:36 -0400

    All tests pass. probably need a bit of cleanup yet

--- src/Adv76.JsonMergePatch.Test/Rfc7396Tests.cs
+++ src/Adv76.JsonMergePatch.Test/Rfc7396Tests.cs
@@ -3,6 +3,12 @@ using System.Text.Json.Nodes;
 
 namespace Adv76.JsonMergePatch.Test;
 
+/// <summary>
+/// All tests are derived from the RFC 7396 documentation
+/// </summary>
+/// <remarks>
+/// https://datatracker.ietf.org/doc/html/rfc7396
+/// </remarks>
 [TestClass]
 public sealed class Rfc7396Tests
 {
@@ -98,6 +104,151 @@ public sealed class Rfc7396Tests
         Assert.IsNotNull(result);
 
         var expected = new JsonObject { { "a", new JsonArray([ "b" ]) } };
+        
+        Assert.IsTrue(JsonNode.DeepEquals(expected, result));
+    }
+    
+    [TestMethod]
+    public void TestMethod7()
+    {
+        var obj = new JsonObject { { "a", new JsonObject{ { "b", "c" } } } };
+
+        var patch = new JsonObject { { "a", new JsonObject{ { "b", "d" }, { "c", null } } } };
+
+
+        var result = JsonMergePatch.Merge(obj, patch);
+        
+        Assert.IsNotNull(result);
+
+        var expected = new JsonObject { { "a", new JsonObject{ { "b", "d" } } } };
+        
+        Assert.IsTrue(JsonNode.DeepEquals(expected, result));
+    }
+    
+    [TestMethod]
+    public void TestMethod8()
+    {
+        var obj = new JsonObject { { "a", new JsonObject{ { "b", "c" } } } };
+
+        var patch = new JsonObject { { "a", new JsonArray([ 1 ])} };
+
+        var result = JsonMergePatch.Merge(obj, patch);
+        
+        Assert.IsNotNull(result);
+
+        var expected = new JsonObject { { "a", new JsonArray([ 1 ])} };
+        
+        Assert.IsTrue(JsonNode.DeepEquals(expected, result));
+    }
+    
+    [TestMethod]
+    public void TestMethod9()
+    {
+        var obj = new JsonArray(["a", "b"]);
+
+        var patch = new JsonArray(["c", "d"]);
+
+        var result = JsonMergePatch.Merge(obj, patch);
+        
+        Assert.IsNotNull(result);
+
+        var expected = new JsonArray(["c", "d"]);
+        
+        Assert.IsTrue(JsonNode.DeepEquals(expected, result));
+    }
+    
+    [TestMethod]
+    public void TestMethod10()
+    {
+        var obj = new JsonObject { { "a", "b" } };
+
+        var patch = new JsonArray([ "c" ]);
+
+        var result = JsonMergePatch.Merge(obj, patch);
+        
+        Assert.IsNotNull(result);
+
+        var expected = new JsonArray([ "c" ]);
+        
+        Assert.IsTrue(JsonNode.DeepEquals(expected, result));
+    }
+    
+    [TestMethod]
+    public void TestMethod11()
+    {
+        var obj = new JsonObject { { "a", "foo" } };
+
+        var patch = JsonValue.Create((string?)null); // TODO revisit
+
+        var result = JsonMergePatch.Merge(obj, patch);
+        
+        Assert.IsNull(result);
+
+        var expected = JsonValue.Create((string?)null);
+        
+        Assert.IsTrue(JsonNode.DeepEquals(expected, result));
+    }
+    
+    [TestMethod]
+    public void TestMethod12()
+    {
+        var obj = new JsonObject { { "a", "foo" } };
+
+        var patch = JsonValue.Create("bar");
+
+        var result = JsonMergePatch.Merge(obj, patch);
+        
+        Assert.IsNotNull(result);
+
+        var expected = JsonValue.Create("bar");
+        
+        Assert.IsTrue(JsonNode.DeepEquals(expected, result));
+    }
+    
+    [TestMethod]
+    public void TestMethod13()
+    {
+        var obj = new JsonObject { { "e", null } };
+
+        var patch = new JsonObject { { "a", 1 } };
+
+        var result = JsonMergePatch.Merge(obj, patch);
+        
+        Assert.IsNotNull(result);
+
+        var expected = new JsonObject { { "e", null }, { "a", 1 } };
+        
+        Assert.IsTrue(JsonNode.DeepEquals(expected, result));
+    }
+    
+    [TestMethod]
+    public void TestMethod14()
+    {
+        var obj = new JsonArray([ 1, 2 ]);
+
+        var patch = new JsonObject { { "a", "b" }, { "c", null } };
+
+        var result = JsonMergePatch.Merge(obj, patch);
+        
+        Assert.IsNotNull(result);
+
+        var expected = new JsonObject { { "a", "b" } };
+        
+        Assert.IsTrue(JsonNode.DeepEquals(expected, result));
+    }
+    
+    [TestMethod]
+    public void TestMethod15()
+    {
+        var obj = new JsonObject();
+
+        var patch = new JsonObject { { "a", new JsonObject{ { "bb", new JsonObject{ { "ccc", null } } } } } };
+
+        var result = JsonMergePatch.Merge(obj, patch);
+        
+        Assert.IsNotNull(result);
+
+        var expected = new JsonObject { { "a", new JsonObject{ { "bb", new JsonObject() } } } };
         
         Assert.IsTrue(JsonNode.DeepEquals(expected, result));
     }
--- src/Adv76.JsonMergePatch/JsonMergePatch.cs
+++ src/Adv76.JsonMergePatch/JsonMergePatch.cs
@@ -5,7 +5,30 @@ namespace Adv76.JsonMergePatch;
 
 public static class JsonMergePatch
 {
-    public static JsonObject Merge(JsonObject obj, JsonObject patch)
+    public static JsonNode Merge(JsonNode obj, JsonNode patch)
+    {
+        if (patch is null || patch.GetValueKind() is JsonValueKind.Null)
+        {
+            return JsonValue.Create((string?)null);
+        }
+        else if (patch.GetValueKind() is JsonValueKind.Object)
+        {
+            if (obj is null || obj.GetValueKind() is not JsonValueKind.Object)
+            {
+                obj = new JsonObject();
+            }
+            
+            return MergeObject(obj as JsonObject, patch as JsonObject);
+        }
+        else
+        {
+            return patch.DeepClone();
+        }
+
+        return obj;
+    }
+    
+    public static JsonObject MergeObject(JsonObject obj, JsonObject patch)
     {
         //throw new NotImplementedException();
 
@@ -19,7 +42,7 @@ public static class JsonMergePatch
                 }
                 else
                 {
-                    obj[key] = value.DeepClone();
+                    obj[key] = Merge(obj[key], value);
                 }
             }
         }
